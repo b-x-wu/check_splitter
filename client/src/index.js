38dfc7a8a47720690c639fe0cfe920ce4fd4d995
bruce-x-wu <wu.bruce.x@gmail.com>
2022-12-26 10:27:16 -0700

    feat: implement diner name adding

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,27 @@ import React, { useState } from 'react'
 import { createRoot } from 'react-dom/client'
 
 const DinerNameInput = (props) => {
+    const [dinerName, setDinerName] = useState('')
+
+    const handleDinerNameChange = (event) => {
+        setDinerName(event.target.value)
+    }
+
+    // TODO: implement a way to remove diner names
+
     return (
         <>
             Diner Name Input
+            <ul>
+                {props.dinerNames.map((dinerName, idx) => <li key={idx}>{dinerName}</li>)}
+            </ul>
+            <form>
+                <input type={"text"} onChange={handleDinerNameChange} value={dinerName}></input>
+                <button onClick={(event) => {
+                    props.onAddDinerName(event, dinerName)
+                    setDinerName('')
+                }}>Add Diner</button>
+            </form>
             <button onClick={props.onChangeAppState}>Input Items</button>
         </>
     )
@@ -39,11 +57,25 @@ const ErrorDisplay = (props) => {
 
 const App = () => {
     const [appState, setAppState] = useState('diner-name-input')
+    const [dinerNames, setDinerNames] = useState([])
+
+    const handleAddDinerName = (event, dinerName) => {
+        event.preventDefault()
+        if (dinerName !== '') {
+            const dinerNamesClone = dinerNames.slice()
+            dinerNamesClone.push(dinerName)
+            setDinerNames(dinerNamesClone)
+        }
+    }
 
     let currentAppState;
     switch (appState) {
         case 'diner-name-input':
-            currentAppState = <DinerNameInput onChangeAppState={() => { setAppState('item-input') }} />
+            currentAppState = <DinerNameInput 
+                onChangeAppState={() => { setAppState('item-input') }} 
+                onAddDinerName={handleAddDinerName} 
+                dinerNames={dinerNames}
+            />
             break
         case 'item-input':
             currentAppState = <ItemInput onChangeAppState={() => { setAppState('results-display') }} />
@@ -64,4 +96,4 @@ const App = () => {
 }
 
 const root = createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(<React.StrictMode><App /></React.StrictMode>)
