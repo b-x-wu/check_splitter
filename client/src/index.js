b9dc6f462a64d4b7f9999bfa009c115d5687df01
bruce-x-wu <wu.bruce.x@gmail.com>
2022-12-26 10:10:57 -0700

    feat: implement routing between app states

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createRoot } from 'react-dom/client'
 
-const App = () => {
+const DinerNameInput = (props) => {
     return (
         <>
-            This is a React App...
+            Diner Name Input
+            <button onClick={props.onChangeAppState}>Input Items</button>
+        </>
+    )
+}
+
+const ItemInput = (props) => {
+    return (
+        <>
+            Item Input
+            <button onClick={props.onChangeAppState}>View Results</button>
+        </>
+    )
+}
+
+const ResultsDisplay = (props) => {
+    return (
+        <>
+            Results Display
+            <button onClick={props.onChangeAppState}>Reset</button>
+        </>
+    )
+}
+
+const ErrorDisplay = (props) => {
+    return (
+        <>
+            Error Display
+            <button onClick={props.onChangeAppState}>Reset</button>
+        </>
+    )
+}
+
+const App = () => {
+    const [appState, setAppState] = useState('diner-name-input')
+
+    let currentAppState;
+    switch (appState) {
+        case 'diner-name-input':
+            currentAppState = <DinerNameInput onChangeAppState={() => { setAppState('item-input') }} />
+            break
+        case 'item-input':
+            currentAppState = <ItemInput onChangeAppState={() => { setAppState('results-display') }} />
+            break
+        case 'results-display':
+            currentAppState = <ResultsDisplay onChangeAppState={() => { setAppState('diner-name-input') }} />
+            break
+        default:
+            currentAppState = <ErrorDisplay onChangeAppState={() => { setAppState('diner-name-input') }} />
+            break
+    }
+
+    return (
+        <>
+            {currentAppState}
         </>
     )
 }
